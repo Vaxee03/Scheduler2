891ad0728ae1769f0c1092a9a667ec8e2fdb97fb
Vaxee03 <012jas84@gmail.com>
2024-11-15 02:25:35 +0900

    Feat : Member, Schedule Entity 추가

---
+++ src/main/java/com/example/scheduler2/entity/Member.java
@@ -0,0 +1,37 @@
+package com.example.scheduler2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    public Member() {
+    }
+
+    public Member( String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/scheduler2/entity/Schedule.java
@@ -0,0 +1,40 @@
+package com.example.scheduler2.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "Schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "Member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    public Schedule() {
+    }
+
+    public Schedule(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
