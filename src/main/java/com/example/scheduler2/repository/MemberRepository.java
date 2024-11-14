ab33effdb1c61db3443ec857d8cc92f6d09e8ca1
Vaxee03 <012jas84@gmail.com>
2024-11-15 03:58:16 +0900

    Feat : delete(member) 메서드 추가
    Id 입력하여 DB에서 해당하는 member를 삭제하는 메서드 추가,
    입력한 Id가 존재하지 않을경우 404 코드를 응답하는 메서드 추가

--- src/main/java/com/example/scheduler2/repository/MemberRepository.java
+++ src/main/java/com/example/scheduler2/repository/MemberRepository.java
@@ -2,8 +2,13 @@ package com.example.scheduler2.repository;
 
 import com.example.scheduler2.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
+    default Member findByIdOrElseThrow(Long id){
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
