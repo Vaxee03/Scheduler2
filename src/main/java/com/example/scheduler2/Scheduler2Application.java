abe7802b2478ef523150208d188863014d50dee0
Vaxee03 <012jas84@gmail.com>
2024-11-15 03:22:21 +0900

    Feat : member, schedule의 생성일, 수정일을 저장하는 기능 추가
    Application에 @EnableJpaAuditing 추가,
    DB 데이터 소스 설정 추가

--- src/main/java/com/example/scheduler2/Scheduler2Application.java
+++ src/main/java/com/example/scheduler2/Scheduler2Application.java
@@ -2,8 +2,11 @@ package com.example.scheduler2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Scheduler2Application {
 
     public static void main(String[] args) {
